af6c1591802d8bde05eb8163bb4f37a7c4d28d74
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-06-06 11:41:44 +0700

    header & search in dashboard

--- lib/presentation/pages/dashboard_page.dart
+++ lib/presentation/pages/dashboard_page.dart
@@ -1,4 +1,6 @@
+import 'package:eago_app/presentation/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -8,6 +10,76 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _header(),
+              _search(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 40, 20, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Best Product',
+            style: AppTheme.headline1,
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Perfect Choice!',
+            style: AppTheme.headline3.normal,
+          )
+        ],
+      ),
+    );
+  }
+
+  _search(){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: Icon(
+                  Icons.search_outlined,
+                  color: AppTheme.deepBlue,
+                ),
+                hintText: 'Search',
+                hintStyle: AppTheme.text1
+              ),
+              style: AppTheme.text1.withDeepBlue,
+            ),
+          ),
+          SizedBox(width: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: SvgPicture.asset(Resources.setting, width: 20,),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/utils/constants.dart
+++ lib/presentation/utils/constants.dart
@@ -1,21 +1,22 @@
 class Resources {
-  static const String baseImages = 'assets/images';
-  static const String baseSvg = 'assets/svgs';
+  static const String _baseImages = 'assets/images';
+  static const String _baseSvg = 'assets/svgs';
 
-  static const String bag1 = '$baseImages/bag_1.png';
-  static const String bag2 = '$baseImages/bag_2.png';
-  static const String bag3 = '$baseImages/bag_3.png';
-  static const String bag4 = '$baseImages/bag_4.png';
-  static const String bag5 = '$baseImages/bag_5.png';
-  static const String bag6 = '$baseImages/bag_6.png';
+  static const String bag1 = '$_baseImages/bag_1.png';
+  static const String bag2 = '$_baseImages/bag_2.png';
+  static const String bag3 = '$_baseImages/bag_3.png';
+  static const String bag4 = '$_baseImages/bag_4.png';
+  static const String bag5 = '$_baseImages/bag_5.png';
+  static const String bag6 = '$_baseImages/bag_6.png';
 
-  static const String icon_launcher = '$baseSvg/ic_launcher_splash.svg';
-  static const String onBoard1 = '$baseSvg/on_board_1.svg';
-  static const String onBoard2 = '$baseSvg/on_board_2.svg';
-  static const String onBoard3 = '$baseSvg/on_board_3.svg';
-  static const String onBoard4 = '$baseSvg/on_board_4.svg';
-  static const String onBoard5 = '$baseSvg/on_board_5.svg';
-  static const String onBoard6 = '$baseSvg/on_board_6.svg';
+  static const String icon_launcher = '$_baseSvg/ic_launcher_splash.svg';
+  static const String setting = '$_baseSvg/settings.svg';
+  static const String onBoard1 = '$_baseSvg/on_board_1.svg';
+  static const String onBoard2 = '$_baseSvg/on_board_2.svg';
+  static const String onBoard3 = '$_baseSvg/on_board_3.svg';
+  static const String onBoard4 = '$_baseSvg/on_board_4.svg';
+  static const String onBoard5 = '$_baseSvg/on_board_5.svg';
+  static const String onBoard6 = '$_baseSvg/on_board_6.svg';
 }
 
 class Keys {
